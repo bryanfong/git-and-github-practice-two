d2de222a4518e367335098153c84d3daee427afd
Philippa Johnstone <pecj@hotmail.co.uk>
2015-10-09 23:31:13 +0800

    message added to button after user enters their email

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 // JavaScript to be added
 $("document").ready(function(){
 
-  $("#button-yellow").on("click", function(){prompt("Please enter your email address:")})
+  $("#button-yellow").on("click", function(){
+    prompt("Please enter your email address:")
+    $("#button-yellow").html("thanks for your email");
+  })
+
 });
